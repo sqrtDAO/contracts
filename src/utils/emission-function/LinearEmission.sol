bea571945859f0ed0a7f6941cd4a1f948f9faf78
Ali Ghahremani <alighahremani1377@gmail.com>
2026-06-10 15:05:03 +0330

    feat: Linear emission implementation

---
+++ src/utils/emission-function/LinearEmission.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {
+    IEmissionFunction
+} from "src/utils/emission-function/IEmissionFunction.sol";
+
+contract LinearEmission is IEmissionFunction {
+    function calculate(
+        bytes calldata _curveConfig,
+        uint256 _epochNumber
+    ) external pure returns (uint256 reward) {
+        LinearEmissionConfig memory config = abi.decode(
+            _curveConfig,
+            (LinearEmissionConfig)
+        );
+        int256 iReward = int256(config.base) +
+            (config.slope * int256(_epochNumber));
+
+        if (reward < 0) {
+            return 0;
+        }
+
+        return uint256(reward);
+    }
+}
+
+struct LinearEmissionConfig {
+    uint256 base;
+    int256 slope;
+}
