34c979d6d2db0004b57ad418af58250362b971af
Ali Ghahremani <alighahremani1377@gmail.com>
2026-06-27 23:18:37 +0330

    fix: some other forge lint warnings

--- src/utils/emission-function/LinearEmission.sol
+++ src/utils/emission-function/LinearEmission.sol
@@ -6,12 +6,13 @@ import {IEmissionFunction} from "src/utils/emission-function/IEmissionFunction.s
 contract LinearEmission is IEmissionFunction {
     function calculate(bytes calldata _curveConfig, uint256 _epochNumber) external pure returns (uint256 reward) {
         LinearEmissionConfig memory config = abi.decode(_curveConfig, (LinearEmissionConfig));
+
+        // forge-lint: disable-next-line(unsafe-typecast) epoch numbers are not that big!
         int256 iReward = int256(config.base) + (config.slope * int256(_epochNumber));
 
-        if (iReward < 0) {
-            return 0;
-        }
+        if (iReward < 0) return 0;
 
+        // forge-lint: disable-next-line(unsafe-typecast) I already checked if iReward is positive
         return uint256(iReward);
     }
 }
