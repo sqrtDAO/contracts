9b350cdd1fdf661f3ecf2563bc67e99c418a7438
Ali Ghahremani <alighahremani1377@gmail.com>
2026-06-29 15:32:39 +0330

    feat: transferTo hook added

---
+++ src/hooks/TransferToHook.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {IERC20} from "lib/forge-std/src/interfaces/IERC20.sol";
+
+contract TransferToHook {
+    event Transferred(address to, uint256 amount);
+
+    /**
+     * @notice transfers all msg.sender allowance to specified address
+     * @param _token token that operation
+     * @param _to the receiver address
+     * @dev The caller must have approved this contract to spend _sellToken.
+     */
+    function transferTo(address _token, address _to) external {
+        address sender = msg.sender;
+        IERC20 token = IERC20(_token);
+
+        uint256 amount = token.allowance(sender, address(this));
+        require(amount > 0, "No participation tokens to swap");
+
+        require(token.transferFrom(sender, _to, amount), "transferFrom failed");
+
+        emit Transferred(_to, amount);
+    }
+}
--- src/utils/emission-function/FixedEmission.sol
+++ src/utils/emission-function/FixedEmission.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {IEmissionFunction} from "src/utils/emission-function/IEmissionFunction.sol";
 
 contract FixedEmission is IEmissionFunction {
-    function calculate(bytes calldata _curveConfig, uint256 _epochNumber) external pure returns (uint256 reward) {
+    function calculate(bytes calldata _curveConfig, uint256) external pure returns (uint256 reward) {
         FixedEmissionConfig memory config = abi.decode(_curveConfig, (FixedEmissionConfig));
         return config.amount;
     }
