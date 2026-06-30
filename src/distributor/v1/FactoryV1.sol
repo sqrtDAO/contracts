abb8a88565acd565fd0ef43eb337766a40e66e93
Ali Ghahremani <alighahremani1377@gmail.com>
2026-06-30 17:52:40 +0330

    feat: adjustable protocol fee and receiver address

--- src/distributor/v1/FactoryV1.sol
+++ src/distributor/v1/FactoryV1.sol
@@ -4,17 +4,29 @@ pragma solidity ^0.8.13;
 import {DistributorV1} from "./DistributorV1.sol";
 import {Hook} from "src/utils/Hook.sol";
 import {EmissionFunction} from "src/utils/emission-function/EmissionFunction.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract FactoryV1 {
+contract FactoryV1 is Ownable {
     event NewDistributor(address indexed distributor);
+
+    uint256 protocolFeeInv;
+    address protocolFeeReceiver;
+
+    constructor(address _initialOwner) Ownable(_initialOwner) {}
+
+    function setProtocolFeeInv(uint256 _protocolFeeInv) public onlyOwner {
+        protocolFeeInv = _protocolFeeInv;
+    }
+
+    function setProtocolFeeReceiver(address _protocolFeeReceiver) public onlyOwner {
+        protocolFeeReceiver = _protocolFeeReceiver;
+    }
 
     function createDistributor(
         address _token,
         address _baseToken,
         uint256 _epochDuration,
         uint256 _startTimestamp,
-        uint256 _protocolFeeInv,
-        address _protocolFeeReceiver,
         uint256 _minParticipation,
         uint256 _claimDelaySeconds,
         bool _allowFutureEpochParticipation,
@@ -26,8 +38,8 @@ contract FactoryV1 {
             _baseToken,
             _epochDuration,
             _startTimestamp,
-            _protocolFeeInv,
-            _protocolFeeReceiver,
+            protocolFeeInv,
+            protocolFeeReceiver,
             _minParticipation,
             _claimDelaySeconds,
             _allowFutureEpochParticipation,
