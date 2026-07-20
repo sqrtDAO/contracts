5c4643214eafdc875c871623559837b5b8f7ab81
Ali Ghahremani <alighahremani1377@gmail.com>
2026-07-20 20:23:16 +0330

    forge fmt

--- script/FactoryV1Local.s.sol
+++ script/FactoryV1Local.s.sol
@@ -12,10 +12,8 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract FactoryV1LocalScript is Script {
     function run() external returns (FactoryV1 factory) {
-        uint256 pk = vm.envOr(
-            "PRIVATE_KEY",
-            uint256(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80)
-        );
+        uint256 pk =
+            vm.envOr("PRIVATE_KEY", uint256(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80));
         vm.startBroadcast(pk);
 
         MockPermit2 permit2 = new MockPermit2();
